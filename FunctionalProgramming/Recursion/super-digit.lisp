f68ccea7c6574f24485f012fb6811db4aa8f8d59
sparkecho <echozhz@126.com>
2016-07-08 23:30:25 +0800

    extract summation of digit to function digit-sum

--- FunctionalProgramming/Recursion/super-digit.lisp
+++ FunctionalProgramming/Recursion/super-digit.lisp
@@ -1,9 +1,13 @@
 (defun super-digit (n)
   (cond ((< n 10) n)
-		(t (super-digit (multiple-value-call
-							#'(lambda (sdigit adigit)
-								(+ (funcall #'super-digit sdigit) adigit))
-						  (truncate n 10))))))
+		(t (super-digit (digit-sum n (number-of-digit n))))))
+
+;; i: number of digit
+(defun digit-sum (num i)
+  (if (< i 2)
+	  num
+	  (+ (rem num 10)
+		 (digit-sum (truncate num 10) (- i 1)))))
 
 (defun number-of-digit (num)
   (if (< num 10)
@@ -16,4 +20,4 @@
 				   (t (build-rec (+ (* acc scale) n) n scale (- k 1))))))
 	(build-rec n n (expt 10 (number-of-digit n)) k)))
 
-(format t "~A" (super-digit (build-number (read) (read))))
+(defun run (n k) (format t "~A" (super-digit (build-number n k))))
