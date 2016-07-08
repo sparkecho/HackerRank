86257eaf3bba4840b3acac36fddcc44b1b5ebf11
sparkecho <echozhz@126.com>
2016-07-08 23:56:50 +0800

    optimize according to the question, 2/10 timeout

--- FunctionalProgramming/Recursion/super-digit.lisp
+++ FunctionalProgramming/Recursion/super-digit.lisp
@@ -1,28 +1,19 @@
 (defun super-digit (n)
   (cond ((< n 10) n)
-		(t (super-digit (digit-sum n (number-of-digit n))))))
+		(t (super-digit (digit-sum n)))))
 
-;; i: number of digit
-(defun digit-sum (num i)
-  (labels ((auxilary (acc num i)
-			 (if (< i 1)
+
+(defun digit-sum (num)
+  (labels ((auxilary (acc num)
+			 (if (zerop num)
 				 acc
 				 (auxilary (+ acc (rem num 10))
-						   (truncate num 10)
-						   (- i 1)))))
-	(auxilary 0 num i)))
-
-(defun number-of-digit (num)
-  (labels ((auxilary (acc num)
-			 (if (< num 10)
-				 acc
-				 (auxilary (1+ acc) (truncate num 10)))))
-	(auxilary 1 num)))
+						   (truncate num 10)))))
+	(auxilary 0 num)))
 
 (defun build-number (n k)
-  (labels ((build-rec (acc n scale k)
-			 (cond ((= k 1) acc)
-				   (t (build-rec (+ (* acc scale) n) n scale (- k 1))))))
-	(build-rec n n (expt 10 (number-of-digit n)) k)))
+  (* k (digit-sum n)))
 
 (defun run (n k) (format t "~A" (super-digit (build-number n k))))
+
+(run (read) (read))
